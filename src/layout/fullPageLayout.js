4951e2e214d62e452294013ce0ed53317dc88c4a
“astute08” <wir.pacharapon@gmail.com>
2021-03-09 05:00:07 +0700

    create web application question1

---
+++ src/layout/fullPageLayout.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Layout, Menu, Breadcrumb, Button } from "antd";
+import "antd/dist/antd.css";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from "@ant-design/icons";
+
+const { Header, Footer, Sider, Content } = Layout;
+const { SubMenu } = Menu;
+
+export default () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const onCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+       
+        <Menu theme="dark" mode="inline">
+          <Menu.Item
+            key="1"
+            onClick = {onCollapse}
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            style = {{height: "100%"}}
+          ></Menu.Item>
+
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            BlockChain Defi Yield Farming.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Pancake Design ©2021 Created by Ant Version 4.0.1
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
---
+++ src/test/test1.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import { Col, Row, Input, Select } from "antd";
+import "./test.css";
+
+export default () => {
+  const [inputValue, setInputValue] = useState("");
+  const [selectValue, setSelectValue] = useState("");
+  const [typingTimeout, setTypingTimeout] = useState(0);
+
+  const { Option } = Select;
+
+  useEffect(() => {
+    // console.log("useEffect work");
+    isPrimeFunction();
+    isFibonacciFunction();
+  }, [selectValue, inputValue]);
+
+  const onChangeInput = (e) => {
+    const value = e.target.value;
+    const num = Number(Math.round(value).toFixed(0));
+
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+    
+    setInputValue(value);
+    setTypingTimeout(
+      setTimeout(function () {
+        setInputValue(num);
+      }, 2000)
+    );
+
+  };
+
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+    setSelectValue(value);
+  };
+
+  const isPrimeFunction = () => {
+    if (inputValue !== "" && selectValue !== "") {
+      if (selectValue === "isPrime") {
+        if (inputValue > 1) {
+          for (let i = 2; i <= inputValue; i++) {
+            if (inputValue % i === 0) {
+              return false;
+            } else {
+              return true;
+            }
+          }
+        } else if (inputValue == "0" || inputValue == "1") {
+          return false;
+        }
+      }
+    }
+  };
+
+  function isSquare(n) {
+    return n > 0 && Math.sqrt(n) % 1 === 0;
+  }
+
+  const isFibonacciFunction = () => {
+    var i;
+    var fib = [];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    if (inputValue !== "" && selectValue !== "") {
+      const num = parseInt(inputValue);
+      if (isSquare(5 * (num * num) - 4) || isSquare(5 * (num * num) + 4)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+
+  const resultOfPrime = JSON.stringify(isPrimeFunction());
+  const resultOfFibonacci = JSON.stringify(isFibonacciFunction());
+  const result = selectValue === "isPrime" ? resultOfPrime : resultOfFibonacci;
+  // console.log(Number((6.688689).toFixed(1)));
+
+  return (
+    <div>
+      <table className="table">
+        <tr>
+          <th className="col1">
+            <Input value={inputValue} onChange={onChangeInput} />
+          </th>
+          <th className="col2">
+            <Select
+              style={{ width: 120 }}
+              value={selectValue}
+              onChange={handleChange}
+            >
+              <Option value="isPrime">isPrime</Option>
+              <Option value="isFibonacci">isFibonacci</Option>
+            </Select>
+          </th>
+          <th className="col3">{result}</th>
+        </tr>
+      </table>
+    </div>
+  );
+};
